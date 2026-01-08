9f3072cfb6b8722454b0a03909aa7b6b7e7f0eef
INexxusI <fbourigault@yahoo.com>
2026-01-08 10:10:37 -0500

    Update base.php

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -106,14 +106,7 @@
 </head>
 <body>
 
-  <nav class="navbar">
-    <div class="container d-flex align-items-center justify-content-between py-2">
-      <a class="navbar-brand fw-bold" href="/">TDG</a>
-      <div class="d-flex gap-2">
-        <a class="btn btn-sm btn-ghost" href="/lesson">Leçon</a>
-      </div>
-    </div>
-  </nav>
+require __DIR__ . '/../partials/navbar.php';
 
   <main class="py-4">
     <div class="container">
