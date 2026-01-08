1447ebf25366efdcbfa6ada5ee6cda42e35b2425
INexxusI <fbourigault@yahoo.com>
2026-01-08 09:56:51 -0500

    Create navbar.php

---
+++ app/Views/partials/navbar.php
@@ -0,0 +1,8 @@
+  <nav class="navbar">
+    <div class="container d-flex align-items-center justify-content-between py-2">
+      <a class="navbar-brand fw-bold" href="/">TDG</a>
+      <div class="d-flex gap-2">
+        <a class="btn btn-sm btn-ghost" href="/lesson">Leçon</a>
+      </div>
+    </div>
+  </nav>
